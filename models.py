015cf7b9d0ca543914631ecdaf62c47903bac437
Alex D'Souza <113999925+AlexanderDsouza@users.noreply.github.com>
2026-01-19 12:13:11 -0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+from torch.nn import Module
+import torch.nn.functional as F
+import math
+
+class SGC(nn.Module):
+    """
+    A Simple PyTorch Implementation of Logistic Regression.
+    Assuming the features have been preprocessed with k-step graph propagation.
+    """
+    def __init__(self, nfeat, nclass, dropout, use_relu):
+        super(SGC, self).__init__()
+
+        self.W = nn.Linear(nfeat, nclass)            
+        self.dropout = dropout # added dropout
+        self.use_relu = use_relu
+
+    def forward(self, x):
+        x = self.W(x)
+        if self.use_relu:
+            x = F.relu(x) # added reLU
+        x = F.dropout(x, self.dropout, training=self.training)
+        return x
+
+class GraphConvolution(Module):
+    """
+    A Graph Convolution Layer (GCN)
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.W = nn.Linear(in_features, out_features, bias=bias)
+        self.init()
+
+    def init(self):
+        stdv = 1. / math.sqrt(self.W.weight.size(1))
+        self.W.weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        support = self.W(input)
+        output = torch.spmm(adj, support)
+        return output # This was missing in SGC code
+
+class GCN(nn.Module):
+    """
+    A Two-layer GCN.
+    """
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        print("x:", len(x), len(x[0]))
+        x = self.gc1(x, adj)
+        #print("X", x)
+        
+        x = F.relu(x)
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return x
+
+def get_model(model_opt, nfeat, nclass, nhid, dropout, cuda, use_relu):
+    if model_opt == "GCN":
+        model = GCN(nfeat=nfeat,
+                    nhid=nhid,
+                    nclass=nclass,
+                    dropout=dropout)
+    elif model_opt == "SGC":
+        model = SGC(nfeat=nfeat,
+                    nclass=nclass, dropout=dropout, use_relu=use_relu) # added dropout
+    else:
+        raise NotImplementedError('model:{} is not implemented!'.format(model_opt))
+
+    if cuda: model.cuda()
+    return model
